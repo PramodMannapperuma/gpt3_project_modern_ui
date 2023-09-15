51af07735b6d36d2514881f5d2b726fba40001b5
Pramod Mannapperuma <96408353+PramodMannapperuma@users.noreply.github.com>
2023-09-15 21:49:44 +0530

    Added navbar and all the src files

---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CTA = () => {
+  return (
+    <div><h1>CTA</h1></div>
+  )
+}
+
+export default CTA
---
+++ src/containers/possibility/Possibility.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Possibility = () => {
+  return (
+    <div><h1>Possibility</h1></div>
+  )
+}
+
+export default Possibility
